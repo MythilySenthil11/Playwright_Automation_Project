d6d396cbd73c9429a71c8c607a1beaa9468cc4e8
Mythilysenthil <mythilsenthilp@gamil.com>
2026-07-07 12:31:34 +0530

    Project Structure created

---
+++ cucumber.js
@@ -0,0 +1,46 @@
+module.exports = {
+  default: {
+    requireModule: ["ts-node/register"],
+
+    require: [
+      "src/test/hooks/**/*.ts",
+      "src/test/steps/**/*.ts"
+    ],
+
+    paths: ["src/test/features/**/*.feature"],
+
+    publishQuiet: true,
+
+    format: [
+      "progress",
+      "summary",
+      "json:reports/cucumber-report.json",
+      "html:reports/cucumber-report.html",
+      "rerun:@rerun.txt",
+      "allure-cucumberjs/reporter"
+    ],
+
+    parallel: 1
+  },
+
+  rerun: {
+    requireModule: ["ts-node/register"],
+
+    require: [
+      "src/test/hooks/**/*.ts",
+      "src/test/steps/**/*.ts"
+    ],
+
+    paths: ["@rerun.txt"],
+
+    publishQuiet: true,
+
+    format: [
+      "progress",
+      "json:reports/rerun-cucumber-report.json",
+      "html:reports/rerun-cucumber-report.html"
+    ],
+
+    parallel: 1
+  }
+};
