d743bcff932c96afa54eb1515ec0610c2c8ff6bd
krishnaprasath <prasathkrishna002@gmail.com>
2026-07-09 20:33:32 +0530

    allure configuration

--- cucumber.js
+++ cucumber.js
@@ -6,28 +6,33 @@ module.exports = {
             "src/test/steps/**/*.ts",
             "src/test/hooks/**/*.ts"
         ],
-        order: "defined",
+
         paths: [
             "src/test/features/**/*.feature"
         ],
 
+        order: "defined",
+
         formatOptions: {
             snippetInterface: "async-await",
-            resultsDir: "allure-results" // 1. Explicitly point to the target folder
+            resultsDir: "allure-results"
         },
 
         publishQuiet: true,
         dryRun: false,
+
         format: [
-            "allure-cucumberjs/reporter:./reports/allure-dummy.txt", // 2. Redirect stdout to avoid conflicts with 'progress'
+            "progress",
+            "allure-cucumberjs/reporter",
             "json:reports/cucumber-report.json",
             "html:reports/cucumber-report.html",
             "message:reports/messages.ndjson",
-            "rerun:rerun/@rerun.txt",
-            "progress"
+            "rerun:rerun/@rerun.txt"
         ],
+
         parallel: 1
     },
+
     rerun: {
         requireModule: ["ts-node/register"],
 
@@ -36,21 +41,29 @@ module.exports = {
             "src/test/hooks/**/*.ts"
         ],
 
+        paths: [
+            "@rerun.txt"
+        ],
+
+        order: "defined",
+
         formatOptions: {
             snippetInterface: "async-await",
-            resultsDir: "allure-results" // 1. Explicitly point to the target folder here too
+            resultsDir: "allure-results"
         },
 
         publishQuiet: true,
         dryRun: false,
+
         format: [
             "progress",
-            "allure-cucumberjs/reporter:./reports/allure-dummy.txt", // 2. Redirect stdout here too
+            "allure-cucumberjs/reporter",
             "json:reports/cucumber-report.json",
             "html:reports/cucumber-report.html",
             "message:reports/messages.ndjson",
             "rerun:rerun/@rerun.txt"
         ],
+
         parallel: 1
     }
 };
