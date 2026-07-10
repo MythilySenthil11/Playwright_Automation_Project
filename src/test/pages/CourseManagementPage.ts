bedb4f929ad0c83317e5e9a83f731b63685ffff6
Jerishwin Joseph <jerishwinj@gmail.com>
2026-07-10 12:15:11 +0530

    used csv for edit feature

--- src/test/pages/CourseManagementPage.ts
+++ src/test/pages/CourseManagementPage.ts
@@ -2,6 +2,8 @@ import { logger } from './../utilities/logger';
 import { Locator, Page } from "@playwright/test";
 import { BasePage } from "./BasePage";
 import { TIMEOUTS } from '../constants/timeouts';
+import { CourseData } from "../types/editCourse.types";
+
 
 export class CourseManagementPage extends BasePage{
     readonly page:Page;
@@ -150,16 +152,17 @@ export class CourseManagementPage extends BasePage{
         await this.Click(this.previewButton)
     }
 
-    async fillFirstPage(){
-        await this.SelectCustomDropdown(this.courseClientDropDown, 'jamocha');
-        await this.SelectCustomDropdown(this.serviceTypeDropDown, 'Automation Testing');
-        await this.SelectCustomDropdown(this.serviceModelDropDown, 'Automation');
-        await this.SelectCustomDropdown(this.courseCategoryDropDown, 'Software Development');
-        await this.SelectCustomDropdown(this.courseNameDropDown, 'Frontend');
+    async fillFirstPage(data: CourseData) {
+
+        await this.SelectCustomDropdown(this.courseClientDropDown,data.courseClient);
+        await this.SelectCustomDropdown(this.serviceTypeDropDown,data.serviceType);
+        await this.SelectCustomDropdown(this.serviceModelDropDown,data.serviceModel);
+        await this.SelectCustomDropdown(this.courseCategoryDropDown,data.courseCategory);
+        await this.SelectCustomDropdown(this.courseNameDropDown,data.courseName);
     }
 
-    async fillSecondPage(){
-        await this.SelectCustomDropdown(this.courseLevelDropDown,'Beginner')
+    async fillSecondPage(data: CourseData){
+        await this.SelectCustomDropdown(this.courseLevelDropDown,data.courseLevel);
         // await this.UploadFile(this.uploadphotoButton,'Playwright_Automation_Project\\src\\test\\test-data\\logo.png')
     }
 
